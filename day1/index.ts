052a5102c81ae4435873a46b30c5ebb848d959ff
Thomas Huber <me@tohuber.com>
2022-12-01 01:43:31 -0700

    Day 1 :christmas_tree:

--- day1/index.ts
+++ day1/index.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs'
 
-let dataString = fs.readFileSync('./day1/input.txt', { encoding: 'utf-8' })
+let dataString = fs.readFileSync('./day1/input.txt',
+    { encoding: 'utf-8' })
 
 let data = dataString.split('\n')
 let calSums = (new Array(data.length)).fill(0)
@@ -9,5 +10,27 @@ data.forEach(s => {
     if (s === '') calSumsI++
     else calSums[calSumsI] += parseInt(s)
 })
-console.log(calSums.reduce((prev, curr) => { return curr > prev ? curr : prev }));
+
+console.log(`highest calorie count: ${calSums.reduce(
+    (prev, curr) => { return curr > prev ? curr : prev })}`);
+
+let top3 = [0, 0, 0]
+calSums.forEach(c => { // long but best O(n) 🤷‍♂️
+    let usedC = false
+    for (let i = 0; i < 3; i++)
+        if (c > top3[i] && !usedC) {
+            let usedOld = false
+            for (let j = i; j < 3; j++) {
+                if (top3[i] > top3[j] && !usedOld) {
+                    top3[j] = top3[i]
+                    usedOld = true
+                }
+            }
+            top3[i] = c
+            usedC = true
+        }
+})
+
+console.log(`highest 3 calorie counts: ${top3}, sum: ${top3.reduce(
+    (prev, curr) => { return curr += prev })}`);
 
